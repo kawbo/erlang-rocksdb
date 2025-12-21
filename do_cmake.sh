685552aa72632970191807efb1a7ab9fa8cac542
Oleksii Semilietov <spylik@gmail.com>
2025-12-21 13:02:39 +0700

    cmake

--- do_cmake.sh
+++ do_cmake.sh
@@ -9,10 +9,12 @@ fi
 mkdir -p _build/cmake
 cd _build/cmake
 
-if type cmake3 > /dev/null 2>&1 ; then
-    CMAKE=cmake3
-else
-    CMAKE=cmake
+if [ -z "$CMAKE" ]; then
+    if type cmake3 > /dev/null 2>&1 ; then
+        CMAKE=cmake3
+    else
+        CMAKE=cmake
+    fi
 fi
 
 ${CMAKE} "$@" ../../c_src || exit 1
